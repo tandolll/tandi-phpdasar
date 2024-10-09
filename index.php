37374a7fa7079ad6e9ec2bf156ee68c17942c717
tandolll <tandi.nugroho7@smk.belajar.id>
2024-10-09 16:33:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+if (isset($_POST["cari"])) {
+$mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Halaman Admin</title>
+</head>
+<body>
+
+    <h1>Daftar Mahasiswa</h1>
+<a href="tambah.php">Tambah data mahasiswa</a>
+<br><br>
+<form action="" method="post">
+	<input type="text" name="keyword" size="40" autofocus placeholder="Cari data mahasiswa" autocomplete="off"><button type="submit" name="cari">Cari</button>
+<br><br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+ 
+    <tr>
+        <th>NO.</th>
+        <th>AKSI</th>
+        <th>GAMBAR</th>
+        <th>NRP</th>
+        <th>NAMA</th>
+        <th>EMAIL</th>
+        <th>JURUSAN</th>
+</tr>
+<?php $i = 1; ?>
+<?php foreach( $mahasiswa as $row ) : ?>
+   <tr>
+  
+    <td><?= $i; ?></td>
+    <td>
+        <a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> | 
+        <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin mau dihapus?');">hapus</a>
+    </td>
+    <td><img src="img/<?= $row["gambar"]; ?>" width="100"></td>
+    <td><?= $row["nrp"]; ?></td>
+    <td><?= $row["nama"]; ?></td>
+    <td><?= $row["email"]; ?></td>
+    <td><?= $row["jurusan"]; ?></td>
+   </tr>
+   <?php $i++; ?>
+<?php endforeach; ?> 
+
+
+</table>
+</body>
+</html>
